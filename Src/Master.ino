c32be1140d55fa91fd71957b0d0d6e1500d40472
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2024-03-06 18:28:37 +0700

    Update Master.ino

--- Src/Master.ino
+++ Src/Master.ino
@@ -1,107 +1,69 @@
 // library initialization
 #include <ESP8266WiFi.h> // calls a library called ESP8266WiFi
-#include <PubSubClient.h> // calls a library called PubSubClient
-#include <ArduinoJson.h> // calls a library called ArduinoJson
-
-// object initialization
-WiFiClient espClient; // create a new object with the name: espClient
-PubSubClient client(espClient); // create a new object with the name: client
-StaticJsonDocument<256> DataPublish; // create a new object with the name: DataPublish
 
 // variable initialization
-#define ssid "YOUR_WIFI_NAME" // router name
-#define password "YOUR_WIFI_PASSWORD" // router password
-#define mqtt_server "IOT_PLATFORM" // mqtt server
-#define mqtt_topic "Moisture Sensor" // mqtt topic
-String clientId = "YOUR_CLIENT_ID"; // client id from IoT Platform
-char Payload[128]; // data with character type is used for payload purposes
-#define wetSoil 277 // maximum value considered as 'wet' soil
-#define drySoil 380 // minimum value considered as 'dry' soil
-int moisture; // data with integer type is used for moisture sensor purposes
-String status; // data with String type is used for moisture sensor purposes
+String data;
+boolean StringReady = false;
 
-// Method: setup
-void setup(){
-  Serial.begin(115200); // start serial communication inside the ESP-01
-  WiFiconnection(); // calling the WiFiconnection method
-  client.setServer(mqtt_server, 1883); // set server & port
+void setup() {
+  Serial.begin(9600); // start serial communication inside the ESP-01
   while(!Serial){ // if serial communication is not connected then do :
     ; // wait for serial port to connect
   }
 }
 
-// Method: loop
-void loop(){
+void loop() { 
   dataRetrieval(); // calling the dataRetrieval method
-  soilCondition(); // calling the soilCondition method
-  if (!client.connected()) { reconnect(); } // if the client is not connected then run the method called reconnect
-  client.loop(); // if the client is not connected then client requests will be conducted continuously
-  dataPublish(); // calling the dataPublish method
 }
 
 // Method: WiFiconnection
-void WiFiconnection(){
-  delay(500); // time delay in loop
-  Serial.print("Connecting to WiFi: "); Serial.println(ssid); // print to serial monitor
-  WiFi.begin(ssid, password); // starting wifi
-  while(WiFi.status() != WL_CONNECTED){ // while not successfully connect to the WiFi then :
-    delay(500); // time delay in loop
-    Serial.print("."); // print to serial monitor continuously
-  }     
+void WiFiconnection(String ssid, String password){
+  WiFi.begin(ssid, password); // starting wifi   
   if(WiFi.status() == WL_CONNECTED){ // if successfully connect to the WiFi then :
-    Serial.println(""); Serial.println("Connected to WiFi !!"); Serial.print("IP address : "); Serial.println(WiFi.localIP()); Serial.println("\n"); // print to serial monitor
+    Serial.println("\nWiFi Connected !!"); // send response to Arduino Uno
   }
   else{ // if not successfully connect to the WiFi then : 
-    Serial.println("\nFailed to connect to WiFi !!"); // print to serial monitor
+    while(WiFi.status() != WL_CONNECTED){ // while not successfully connect to the WiFi then :
+      delay(500); // time delay in loop
+      Serial.print("."); // send response to Arduino Uno
+    }  
   } 
-}
-
-// Method: reconnect
-void reconnect() {
-  while(!client.connected()) { // while client not successfully connect to the server then :
-    Serial.print("Attempting MQTT connection..."); // print to serial monitor
-    clientId += String(random(0xffff), HEX); // Create a random client ID to make it different from other people's client ID
-    if(client.connect(clientId.c_str())){ // if client successfully connect to the server then :
-      Serial.println("Connected"); // print to serial monitor
-    } else { // if client not successfully connect to the server then :
-      Serial.print("failed, rc="); Serial.print(client.state()); Serial.println("try again in 5 seconds"); // print to serial monitor
-      delay(5000); // time delay in loop
-    }
-  }
 }
 
 // Method: dataRetrieval
 void dataRetrieval(){
+  data = ""; // this String data type is used to store data obtained from serial communication
   if(Serial.available()){ // if serial communication is connected then do :
     while(Serial.available()){ // this loop is used to read the serial communication data from the Arduino Uno
-      moisture += char(Serial.read()); // adds each sensor data reading into a data string named moisture
+      data += Serial.readString(); // adds each sensor data reading into a data string named data
+      StringReady= true;
     }
-    delay(5000); // time delay in loop
-    Serial.println("\nAnalog value: "); Serial.println(moisture); // print to serial monitor
+    if (StringReady){
+      data.trim(); // remove existing spaces
+      String ssid = getValue(data, ',', 0); // this variable is used to store ssid data
+      String password = getValue(data, ',', 1); // this variable is used to store password data
+      WiFiconnection(ssid, password);
+    }
+    delay(1000);
   }
 }
 
-// Method: soilCondition
-void soilCondition(){
-  // determine status of soil
-  if (moisture < wetSoil) { // if the sensor value is less than 277 then :
-    status = "wet"; // wet soil conditions
-    Serial.println("Status: "+String(status)); // print to serial monitor
-  } else if (moisture >= wetSoil && moisture < drySoil) { // if the sensor value is within the range of 277 - 380 then :
-    status = "moist"; // moist soil conditions
-    Serial.println("Status: "+String(status)); // print to serial monitor
-  } else { // if the sensor value is not in wet and moist conditions then :
-    status = "dry"; // dry soil conditions
-    Serial.println("Status: "+String(status)); // print to serial monitor
-  }
-}
+// function for data parsing
+String getValue(String data, char separator, int index){ // there are 3 parameters: data -> string, separator -> character, index -> integer
+  int found = 0; // variable with integer type to store found data
+  int strIndex[] = {0, -1}; // variable with integer type to store two index values to be used as bounds when extracting substrings from the data string. the initial limit is 0, and the final limit is the final number read and then subtracted by 1
+  int maxIndex = data.length()-1; // variable with integer type to store the upper limit of the index
+ 
+  for(int i=0; i<=maxIndex && found<=index; i++){ // loop to search data
+    if(data.charAt(i)==separator || i==maxIndex){ // if the character at index-i in the data string is equal to a separator character or index-i has reached the end of the string then
+        found++; // increases the number of values that have been found during iteration through the string
+        strIndex[0] = strIndex[1]+1; // to set the initial border of the substring that will be retrieved
+        strIndex[1] = (i == maxIndex) ? i+1 : i; // to set the end limit of the substring to be retrieved
+    }
+  } 
 
-// Method: dataPublish
-void dataPublish(){
-  DataPublish["value"] = String(moisture); // this data array is used to store the value of the soil moisture sensor measurement results
-  DataPublish["status"] = String(status); // this data array is used to store the status of the soil moisture sensor measurement results
-  serializeJson(DataPublish, Payload); // produces JSON data output without indentation
-  boolean dataout = client.publish(mqtt_topic, Payload); // data published
-  Serial.println("\n----------------------------------------------"); Serial.println("Json Packages: " + String(Payload)); Serial.println("Publish Information: Data Sent"); Serial.println("----------------------------------------------"); // print to serial monitor
-  delay(2000); // time delay in loop
+  // returns a value based on the condition:
+  // 1. checks whether the value found is greater than the index. if this condition is true, then the function will return the substring data starting from strIndex[0] to strIndex[1]
+  // 2. if the condition is false (meaning the found value is not greater than the index), then the function will return an empty string meaning no value was found at the requested index
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
